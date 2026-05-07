c2085ec86dcbfb66b4b39b291ad7eb35fb4d8e59
CNote <console.notebook.app@gmail.com>
2026-05-07 20:43:00 +0300

    ebay fix

--- backend/providers/EbayProvider.js
+++ backend/providers/EbayProvider.js
@@ -35,7 +35,11 @@ class EbayProvider extends MarketplaceProvider {
 		state,
 
 		// MINIMAL SCOPE
-		scope: 'https://api.ebay.com/oauth/api_scope'
+		scope: [
+			'https://api.ebay.com/oauth/api_scope',
+			'https://api.ebay.com/oauth/api_scope/sell.inventory',
+			'https://api.ebay.com/oauth/api_scope/sell.account.readonly'
+		].join(' ')
 	});
 
 	const url =
